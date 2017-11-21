0d2828fe090f983a625f7d5412b21d5fe213e196
liyang@yodo1.com <liyang@yodo1.com>
2017-11-21 18:51:03 +0800

    net test 3 timeserver time client

---
+++ netty-03/src/main/java/server/TimeServer.java
@@ -0,0 +1,43 @@
+package server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+
+    public void bind(int port) {
+       NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+       NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+       ServerBootstrap b = new ServerBootstrap();
+        try {
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,1024)
+                    .childHandler(new ChildChannelHandler());
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        int port = 8080;
+        if(args != null && args.length > 0){
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
+        new TimeServer().bind(port);
+    }
+}
